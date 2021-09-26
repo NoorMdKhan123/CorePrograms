45dd217a23df96f4646d0d3a6a45c82932bcf0f0
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 02:11:52 +0530

    Claculating largest number amongst 3 numbers

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreatestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 3rd number");
+            int num3 = int.Parse(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("1st number is greatest among all three");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("2nd number is greatest among all three");
+            }
+            else
+            {
+                Console.WriteLine("3rd number is greatest among all three");
+
+            }
+        }
+    }
+}
