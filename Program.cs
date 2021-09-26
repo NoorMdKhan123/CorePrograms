e479cd42a6e5ebbe9e1755107acf488632ec4538
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 01:49:48 +0530

    Power of 2 program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TableProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter power");
+            int power = int.Parse(Console.ReadLine());
+            int p = 1;
+            Console.WriteLine("The Table of power 2 is : ");
+       
+            for(int i=1;i<=power;i++)
+            {
+                p = p * 2;
+                Console.WriteLine("2^" +i+ "="+ p);
+            }
+        }
+    }
+}
