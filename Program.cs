9dcfd38ca687d5de754d202374e6ba312f6c1dd8
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 01:56:16 +0530

    Calulated all prime factors of given number

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number whose factors has to be find");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The factors are : ");
+            for (int i = 2; num > 1; i++)
+            {
+                
+                while (num % i == 0)
+                {
+                    
+                    Console.Write(" "+i+ " ");
+
+                  num /= i;
+
+                }
+
+            }
+          
+        }
+    }
+}
