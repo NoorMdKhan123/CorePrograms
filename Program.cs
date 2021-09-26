37b1aca558cc4746fe428c10510435e4d8682a33
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 01:57:58 +0530

    Calculated quitoent and remainder of a given number

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuotientRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dividend number");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the divisor number");
+            int divisor = int.Parse(Console.ReadLine());
+            int quoitent = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine(" the quoitent is"+ quoitent);
+            Console.WriteLine(" the remainder is" + remainder);
+
+
+
+
+        }
+    }
+}
