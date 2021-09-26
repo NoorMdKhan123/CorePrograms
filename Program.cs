2bda7e4749d2b2fbd8ea75dbdbf4eb87ae424bc0
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 01:52:30 +0530

    Printed Harmonic series till nth term

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HarmonicSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num = int.Parse(Console.ReadLine());
+            
+            string sum = "1";
+            Console.WriteLine("the nth series is");
+            for(int i=2; i<num; i++)
+            { 
+                sum += "+(1/" + i + ")";
+                
+               
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
