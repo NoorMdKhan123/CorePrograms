476005c5b340b5c13d850285d3ebcb989d35b942
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 02:06:24 +0530

    Checking consonant or vowel from given character

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsonantVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character to check if it's a conconant or vowel");
+            char character = char.Parse(Console.ReadLine());
+            
+                if (character == 'a' || character == 'e' || character == 'i' ||
+                    character == 'o' || character == 'u' || character == 'A' || character == 'E'
+                    || character == 'I' || character == 'O' || character == 'U')
+                {
+                    Console.WriteLine("Enter charatcer is a vowel");
+                }
+                else
+                {
+                    Console.WriteLine("Enter charatcer is a consonant");
+                }
+            
+        
+        }
+    }
+}
