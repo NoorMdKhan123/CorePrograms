5fc82f91edaebc55a49ea3be0e2c5b32efdc586e
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 01:46:19 +0530

    Leap year program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeapYearProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check");
+            int year = int.Parse(Console.ReadLine());
+            if( ((year % 4 == 0) && (year % 100 != 0 )) || (year % 400 == 100))
+                    {
+                Console.WriteLine("Year is a leap yeaar");
+            }
+            else
+                {
+                Console.WriteLine("year is not a leap year");
+
+            }
+        }
+    }
+}
